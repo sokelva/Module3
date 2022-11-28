b42489403da7f9f09367503a7093b0fe3e24fec5
sokelva <sokelva@mail.ru>
2022-11-28 21:45:54 +0300

    Программа с вопросами.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,33 +10,39 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            const string MyName = "Елена";
-            Console.WriteLine("Мое имя: {0}", MyName);
-            Console.ReadKey();
+            //const string MyName = "Елена";
+            //Console.WriteLine("Мое имя: {0}", MyName);
+            //Console.ReadKey();
 
-            //-------------------------------------------------------
+            ////-------------------------------------------------------
 
-            const string Name = "\nJane";
-            Console.WriteLine(MyName);
-            Console.WriteLine("Привет,\n мир!");
-            Console.WriteLine("Привет, мир! \n Я тебя люблю!");
-            Console.WriteLine("Мне 27 лет, \t а моей сестре 25.");
-            Console.WriteLine("My name is Jane.\n Мое имя Елена.");
+            //const string Name = "\nJane";
+            //Console.WriteLine(MyName);
+            //Console.WriteLine("Привет,\n мир!");
+            //Console.WriteLine("Привет, мир! \n Я тебя люблю!");
+            //Console.WriteLine("Мне 27 лет, \t а моей сестре 25.");
+            //Console.WriteLine("My name is Jane.\n Мое имя Елена.");
 
-            Console.WriteLine('\u0040');
-            Console.WriteLine('\u0026');
-            Console.WriteLine('\x23');
-            Console.WriteLine('\x0A');
+            //Console.WriteLine('\u0040');
+            //Console.WriteLine('\u0026');
+            //Console.WriteLine('\x23');
+            //Console.WriteLine('\x0A');
 
 
-            Console.WriteLine(true);
-            Console.WriteLine(false);
-            Console.WriteLine(10);
-            Console.WriteLine(0x0A);
-            Console.WriteLine(0b11);
-            Console.WriteLine(5.5);
-            //Console.WriteLine(null);
+            //Console.WriteLine(true);
+            //Console.WriteLine(false);
+            //Console.WriteLine(10);
+            //Console.WriteLine(0x0A);
+            //Console.WriteLine(0b11);
+            //Console.WriteLine(5.5);
+            ////Console.WriteLine(null);
+            ///
 
+            string MyName = "Елена";
+            byte age = 42;
+            bool isPet = true;
+            double size = 37.5;
+            Console.WriteLine("Мое имя:{0};\nВаш возраст:{1};\nА есть ли у вас домашний питомец:{2};\nВаш размер ноги: {3}", MyName, age, isPet, size);
             Console.ReadKey();
         }
     }
