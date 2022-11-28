a39237dc7626cecbd3e5d1749531f2aed93ca74f
sokelva <sokelva@mail.ru>
2022-11-28 20:59:33 +0300

    #я инициализировал переменную

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName;
+            MyName = "Елена";
+            Console.WriteLine("Мое имя: {0}", MyName);
+        }
+    }
+}
