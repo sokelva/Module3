5b08cf889c3902529e6ea0618834d649395cdc16
sokelva <sokelva@mail.ru>
2022-11-30 00:12:22 +0300

    checked

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -164,10 +164,17 @@ namespace ConsoleApp1
             //Console.WriteLine("Your age is {0} ", age);
             //Console.ReadKey();
 
+            //Console.Write("Enter your name: ");
+            //string name = Console.ReadLine();
+            //Console.Write("Enter your age: ");
+            //byte age = (byte)int.Parse(Console.ReadLine());
+            //Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            //Console.ReadKey();
+
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
             Console.Write("Enter your age: ");
-            byte age = (byte)int.Parse(Console.ReadLine());
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
             Console.WriteLine("Your name is {0} and age is {1} ", name, age);
             Console.ReadKey();
 
